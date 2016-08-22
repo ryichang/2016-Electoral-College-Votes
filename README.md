e58f4d2178ea760f76c1ce8785821a65b78355ed
ryichang <ryichang@gmail.com>
2016-08-22 13:54:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 2016_electoral_college_votes
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.render('public/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
